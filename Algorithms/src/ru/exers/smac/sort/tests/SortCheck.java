98b3e3f61b3b6016fce240e3a0c1fa2db1ace056
smac <sin_s_n@mail.ru>
2016-06-07 00:56:36 +0300

    corrected typo

--- Algorithms/src/ru/exers/smac/sort/tests/SortCheck.java
+++ Algorithms/src/ru/exers/smac/sort/tests/SortCheck.java
@@ -1,6 +1,8 @@
 package ru.exers.smac.sort.tests;
 
 public class SortCheck {
+	
+	
 	public static <T extends Comparable<T>> boolean isSortediDescending(T[] array) {
 		boolean result = true;
 		for (int i = 1; result && i < array.length; i++) {
