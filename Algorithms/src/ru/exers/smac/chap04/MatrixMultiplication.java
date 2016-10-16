20b610aad1c83f37bf44aa8204df6379648ce70a
Sergey Sinitsyn <sin_s_n@mail.ru>
2016-10-16 21:49:16 +0300

    created checkMatrices() and isRectangleMatrix() methods

--- Algorithms/src/ru/exers/smac/chap04/MatrixMultiplication.java
+++ Algorithms/src/ru/exers/smac/chap04/MatrixMultiplication.java
@@ -39,8 +39,7 @@ public class MatrixMultiplication {
 	
 	public static int[][] iterMul(final int[][] matrixA, final int[][] matrixB) {
 		int[][] result = null;
-		if (matrixA != null && matrixB != null && matrixA.length > 0 
-				&& matrixA[0].length == matrixB.length ) {
+		if (checkMatrices(matrixA, matrixB)) {
 			result = new int[matrixA.length][matrixB[0].length];
 			for (int i = 0; i < result.length; i++) {
 				for (int j = 0; j < result[0].length; j++) {
@@ -54,9 +53,37 @@ public class MatrixMultiplication {
 		return result;
 	}
 	
+	private static boolean checkMatrices(int[][] matrixA, int[][] matrixB) {
+		final boolean result;
+		if (matrixA == null || matrixB == null) {
+			result = false;
+		} else {
+			result = isRectangleMatrix(matrixA) && isRectangleMatrix(matrixB)
+					&& (matrixA[0].length == matrixB.length);
+		}
+		return result;
+	}
+
+	private static boolean isRectangleMatrix(int[][] matrix) {
+		if (matrix.length < 1) {
+			return false;
+		}
+
+		final int columnsCount = matrix[0].length;
+		if (columnsCount < 1) {
+			return false;
+		}
+		for (int i = 1; i < matrix.length; i++) {
+			if (columnsCount != matrix[i].length) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public static void main(String...args) {
-		int[][] matrixA = new int[2][3];
-		int[][] matrixB = new int[3][4];
+		int[][] matrixA = new int[2][2];
+		int[][] matrixB = new int[2][2];
 		matrixA[0][0] =  1;
 		matrixA[0][1] =  3;
 		matrixA[1][0] =  7;
@@ -66,9 +93,10 @@ public class MatrixMultiplication {
 		matrixB[1][0] =  4;
 		matrixB[1][1] =  2;
 		int[][] multiplied = iterMul(matrixA, matrixB);
-		for (int i = 0; i < multiplied .length; i++) {
+		for (int i = 0; multiplied != null && i < multiplied.length; i++) {
 			System.out.println(Arrays.toString(multiplied[i]));
 		}
+		System.out.println(multiplied);
 	}
 
 }
