6326878a37fa9a104052953e051fd67777a4795a
Sergey Sinitsyn <sin_s_n@mail.ru>
2016-10-29 22:21:58 +0300

    tryed Heap

--- Algorithms/src/ru/exers/smac/structures/TryHeap.java
+++ Algorithms/src/ru/exers/smac/structures/TryHeap.java
@@ -1,6 +1,25 @@
 package ru.exers.smac.structures;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class TryHeap {
 	
+	private static final Random RND = new Random();
+
+	public static void main(String...args) {
+		Integer[] arr = createInegerArray(15, 20);
+		System.out.println(Arrays.toString(arr));
+		Heap<Integer> h = new Heap<>(arr);
+		System.out.println(Arrays.toString(h.getArray()));
+	}			
+
+	private static Integer[] createInegerArray(final int size, final int maxValue) {
+		final Integer[] result = new Integer[size]; 
+		for (int i = 0; i < result.length; i++) {
+			result[i] = RND.nextInt(maxValue);
+		}
+		return result;
+	}
 
 }
